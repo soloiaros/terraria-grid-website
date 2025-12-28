ea9265b8c5167e7dec2137758c37ea1ade9d977b
soloiaros <soloiaros@gmail.com>
2025-12-28 16:54:30 +0300

    Add interactive tiles In this commit, I implemented a system that allows to make some certain areas of the grid interactive. On click event, interactive tiles get swapped for their counterparts

--- grid-placement.js
+++ grid-placement.js
@@ -1,10 +1,15 @@
 const gridContainer = document.querySelector('.grid-container');
 
+
 function placeBlock(row, column) {
   let newCell = document.createElement('div');
   newCell.classList.add('cell');
-  // newCell.style.border = '1px solid white';
   newCell.style.backgroundImage = `url('grid-images/row-${row}-column-${column}.png')`;
+  newCell.dataset.row = row;
+  newCell.dataset.col = column;
+  
+  newCell.addEventListener('click', gridInteract);
+  
   gridContainer.appendChild(newCell);
 }
 
@@ -13,6 +18,39 @@ function populateGrid(width, height) {
     for (let j = 1; j <= width; j++) {
       placeBlock(i, j);
     }
+  }
+}
+
+const stateSwitchers = {
+  door: false,
+}
+
+const interactiveTilesCoordinates = {
+  door: [
+    [5, 30], [5, 31], [5, 32], [6, 30], [6, 31], [6, 32],
+  ],
+}
+
+function gridInteract(event) {
+  let cellCoordinates = [Number(event.target.dataset.col), Number(event.target.dataset.row)];
+  console.log('clicked', cellCoordinates);
+  for (let [key, value] of Object.entries(interactiveTilesCoordinates)) {
+    if (value.some(
+      listCoordinate => {
+        return listCoordinate.every((val, index) => val === cellCoordinates[index])
+      }
+    )) {
+      swapTiles(key);
+      break;
+    }
+  }
+}
+
+function swapTiles(gridObjectName) {
+  stateSwitchers[gridObjectName] = !stateSwitchers[gridObjectName];
+  for (tileCoordinate of interactiveTilesCoordinates[gridObjectName]) {
+    let gridTile = document.querySelector(`[data-col='${tileCoordinate[0]}'][data-row='${tileCoordinate[1]}']`);
+    gridTile.style.backgroundImage = `url('grid-dynamic-tiles/${gridObjectName}/${stateSwitchers[gridObjectName]}/row-${tileCoordinate[1]}-column-${tileCoordinate[0]}.png')`
   }
 }
 
